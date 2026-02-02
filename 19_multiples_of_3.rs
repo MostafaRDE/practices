e6059c4817a0c3856fe5883cda68f93c6ac238a6
Mostafa Effati <mostafa.rostamdokht@gmail.com>
2026-02-02 03:45:59 -0800

    chore: Adding practice -> adding new way

--- 19_multiples_of_3.rs
+++ 19_multiples_of_3.rs
@@ -1,9 +1,19 @@
 // A program to print multiples of 3 among three given numbers
 
 fn main() {
+    way2();
+}
+
+fn _way1() {
     for i in 100..1000 {
         if i % 3 == 0 {
             println!("{}", i);
         }
+    }
+}
+
+fn way2() {
+    for i in (102..1000).step_by(3) {
+        println!("{}", i);
     }
 }
