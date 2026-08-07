43a1441d6df7b0b12b91a0bcd91b2b7697a0912a
Mostafa Effati <mostafa.rostamdokht@gmail.com>
2026-08-07 09:22:34 -0700

    feat: add rand dependency and implement fetch method in MockProvider

--- rust/rate-core/src/providers/mock.rs
+++ rust/rate-core/src/providers/mock.rs
@@ -1,11 +1,21 @@
 use crate::domain::{
-    provider::{Provider, ProviderError}, rate::Rate,
+    currency::Currency, pair::Pair, provider::{Provider, ProviderError}, rate::Rate,
 };
+use rust_decimal::Decimal;
+use std::time::SystemTime;
 
 pub struct MockProvider {}
 
 impl Provider for MockProvider {
     async fn fetch(&self) -> std::result::Result<Vec<Rate>, ProviderError> {
-        todo!()
+        let buy = Decimal::new(rand::random_range(1_600_000..2_000_000), 0);
+        Ok(vec![
+            Rate {
+                pair: Pair { base: Currency::USD, quote: Currency::IRR },
+                buy,
+                sell: buy * Decimal::new(105, 2).floor(),
+                updated_at: SystemTime::now(),
+            }
+        ])
     }
 }
